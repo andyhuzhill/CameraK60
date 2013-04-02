b5ee2f45328b646289aa2d3206b1b80865fa80fd
Andy Scout <andyhuzhill@gmail.com>
2013-04-02 20:15:25 +0800

    Signed-off-by: Andy Scout <andyhuzhill@gmail.com>

--- Sources/main.c
+++ Sources/main.c
@@ -50,7 +50,7 @@ main(void)
     Size_t imgsize={CAMERA_W,CAMERA_H};         //图像大小 
     Size_t size={LCD_W,LCD_H};                  //显示区域图像大小 
 
-    LCD_Init(RED);
+//    LCD_Init(RED);
 #endif
     ov7725_init(img_bin_buff);
 
@@ -58,8 +58,8 @@ main(void)
     {
         ov7725_get_img();                     //采集图像
 #ifdef DEBUG
-        LCD_Img_Binary_Z(site,size,(uint16 *)img_bin_buff,imgsize);    //显示图像
-        imgResize(img_bin_buff);
+//        LCD_Img_Binary_Z(site,size,(uint16 *)img_bin_buff,imgsize);    //显示图像
+//        imgResize(img_bin_buff);
 #endif
     }
 }
