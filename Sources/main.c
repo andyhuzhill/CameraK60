45f4985a5f6494c5657d5e2296305b3d8b60eb34
caoliang <caoliang0512@gmail.com>
2013-05-02 19:39:20 +0800

    Signed-off-by: caoliang <caoliang0512@gmail.com>

--- Sources/main.c
+++ Sources/main.c
@@ -12,31 +12,6 @@
  * =====================================================
  */
 
-/*  
- *   引脚分配:
- *   PTC14          电机控制使能
- *   PTA8           电机控制PWM1    FTM1 CH0
- *   PTA9           电机控制PWM2    FTM1 CH1
- *   PTC1           舵机控制PWM     FTM0 CH0
- *   PTA11          编码器捕获输入  FTM2 CH1
- *   
- *   PTB0~PTB7      摄像头数据端口
- *   PTB8           PCLK
- *   PTA29          场中断
- *   PTB10~PTB11    摄像头控制端口
- *   
- *   PTA14~PTA17    SPI-2.4G 控制端口
- *   PTE27~PTE28    ？？
- *   
- *   PTD0~PTD7      LCD数据端口
- *   PTC19          LCDRST
- *   PTC9~PTC12     LCD控制端口
- *   
- *   PTC18          SD-Check
- *   PTE0~PTE5      SDcard 接口
- * 
- */
-
 #include "derivative.h" /* include peripheral declarations */
 
 //全局变量定义
