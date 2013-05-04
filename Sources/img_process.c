81914fad33980babdc1bf2b0452b346ad208e99f
Andy Scout <andyhuzhill@gmail.com>
2013-05-04 11:33:37 +0800

    Signed-off-by: Andy Scout <andyhuzhill@gmail.com>

--- Sources/img_process.c
+++ Sources/img_process.c
@@ -55,7 +55,7 @@ imgProcess(void)
     static int ret;
     uint8 status = 0;
     
-    int8 buff[3+IMG_H*3];
+    int8 buff[3];
     
     imgGetImg();
 
@@ -73,17 +73,17 @@ imgProcess(void)
         buff[1] = b;
         buff[2] = k;
         
-        memcpy(&buff[3],middle, IMG_H);
-        memcpy(&buff[3+IMG_H], leftBlack, IMG_H);
-        memcpy(&buff[3+IMG_H*2], rightBlack, IMG_H);
-        
-        for (int i = 0; i < IMG_H; ++i) 
-        {
-            printf("%5d,", leftBlack[i]);
-            printf("%5d,", middle[i]);
-            printf("%5d\n",rightBlack[i]);
-        }
-        printf("\n");
+//        memcpy(&buff[3],middle, IMG_H);
+//        memcpy(&buff[3+IMG_H], leftBlack, IMG_H);
+//        memcpy(&buff[3+IMG_H*2], rightBlack, IMG_H);
+//        
+//        for (int i = 0; i < IMG_H; ++i) 
+//        {
+//            printf("%5d,", leftBlack[i]);
+//            printf("%5d,", middle[i]);
+//            printf("%5d\n",rightBlack[i]);
+//        }
+//        printf("\n");
         
         NRF_ISR_Tx_Dat((uint8*)buff, sizeof(buff));
         
