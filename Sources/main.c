dffce0a3afa699c5f0bf1ea477d8f9099b0f78a0
Andy Scout <andyhuzhill@gmail.com>
2013-06-26 16:08:41 +0800

    Signed-off-by: Andy Scout <andyhuzhill@gmail.com>

--- Sources/main.c
+++ Sources/main.c
@@ -39,6 +39,7 @@ main(void)
 	crtl_e com;
 	Site_t site = {0, 0};
 	Size_t size = {80, 60};
+	Site_t strsite = {0,70};
 	int speed = 0;
 	int8 status = 0;
 	FATFS fs;
@@ -49,6 +50,7 @@ main(void)
 	DisableInterrupts;  //关全局中断
 
 	LCD_Init(GREEN);
+	gpio_init(PORT_E, 6, Mode_IN, High);
 
 #ifdef AT2401
 	NRF_Init();
@@ -66,22 +68,27 @@ main(void)
 			switch(com){
 			case COM_IMG:
 				LCD_Img_Binary(site, size, (uint16*)img_bin_buff);
-	
-//				f_mount(0,&fs);
-//				res = f_open(&file, "0:/img.img", FA_OPEN_ALWAYS | FA_WRITE | FA_READ);
-//
-//				filesize = f_size(&file);
-//
-//				f_lseek(&file, filesize);
-//
-//				f_printf(&file, "img\n");
-//
-//				for(int i=0; i< 600; i++){
-//					f_printf(&file, "%d,", img_bin_buff[i]);
-//				}
-//				f_printf(&file, "\n");
-//
-//				f_close(&file);
+				LCD_Str(strsite,(uint8 *)"              ", BLUE, GREEN);
+
+				if(!(GPIOE_PDIR & (1 << 6))){
+					f_mount(0,&fs);
+					res = f_open(&file, "0:/img.img", FA_OPEN_ALWAYS | FA_WRITE | FA_READ);
+
+					filesize = f_size(&file);
+
+					f_lseek(&file, filesize);
+
+					f_printf(&file, "img\n");
+
+					for(int i=0; i< 600; i++){
+						f_printf(&file, "%d,", img_bin_buff[i]);
+					}
+					f_printf(&file, "\n");
+
+					f_close(&file);
+					
+					LCD_Str(strsite, (uint8 *)"SD write Ok!", BLUE, YELLOW);
+				}
 				break;
 			default:
 				break;
