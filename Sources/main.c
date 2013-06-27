0db4775b5a17262b8d05bb6fab818141a9f2f958
Andy Scout <andyhuzhill@gmail.com>
2013-06-28 00:54:36 +0800

    Signed-off-by: Andy Scout <andyhuzhill@gmail.com>

--- Sources/main.c
+++ Sources/main.c
@@ -36,63 +36,77 @@ ledInit(void)
 int 
 main(void)
 {   
-	crtl_e com;
-	Site_t site = {0, 0};
-	Size_t size = {80, 60};
-	Site_t strsite = {0,70};
-	int speed = 0;
+//	crtl_e com;
+//	Site_t site = {0, 0};
+//	Size_t size = {80, 60};
+//	Site_t strsite = {0,70};
+//	int speed = 0;
 	int8 status = 0;
-	FATFS fs;
-	FIL file;
-	int res;
-	uint32 filesize;
-
-	DisableInterrupts;  //关全局中断
-
-	LCD_Init(GREEN);
-	gpio_init(PORT_E, 6, Mode_IN, High);
+	uint8 rxbuff[4]={0};
+	int8 i;
+//	FATFS fs;
+//	FIL file;
+//	int res;
+//	uint32 filesize;
+//
+//	DisableInterrupts;  //关全局中断
+//
+//	LCD_Init(GREEN);
+//	gpio_init(PORT_E, 6, Mode_IN, High);
 
 #ifdef AT2401
 	NRF_Init();
 #endif
 
 	EnableInterrupts;   //开全局中断
-
-	for (;;) 
+	
+	while(1)
 	{
-		do{
-			status = NRF_MSG_receive(&com, nrf_buff);
-		}while( status == NRF_RESULT_RX_NOVALID);
-
-		if (status == NRF_RESULT_RX_VALID){
-			switch(com){
-			case COM_IMG:
-				LCD_Img_Binary(site, size, (uint16*)img_bin_buff);
-				LCD_Str(strsite,(uint8 *)"              ", BLUE, GREEN);
-
-				if(!(GPIOE_PDIR & (1 << 6))){
-					f_mount(0,&fs);
-					res = f_open(&file, "0:/img.img", FA_OPEN_ALWAYS | FA_WRITE | FA_READ);
-
-					filesize = f_size(&file);
-
-					f_lseek(&file, filesize);
-
-					f_printf(&file, "img\n");
-
-					for(int i=0; i< 600; i++){
-						f_printf(&file, "%d,", img_bin_buff[i]);
-					}
-					f_printf(&file, "\n");
-
-					f_close(&file);
-					
-					LCD_Str(strsite, (uint8 *)"SD write Ok!", BLUE, YELLOW);
-				}
-				break;
-			default:
-				break;
+		status = NRF_ISR_Rx_Dat(rxbuff);
+		
+		if(status == RX_DR){
+			for(i=0;i<4; i++){
+				printf("%d,",rxbuff[i]);
 			}
+			printf("\n");
 		}
+
 	}
+//	for (;;) 
+//	{
+//		do{
+//			status = NRF_MSG_receive(&com, nrf_buff);
+//		}while( status == NRF_RESULT_RX_NOVALID);
+//
+//		if (status == NRF_RESULT_RX_VALID){
+//			switch(com){
+//			case COM_IMG:
+//				LCD_Img_Binary(site, size, (uint16*)img_bin_buff);
+//				LCD_Str(strsite,(uint8 *)"              ", BLUE, GREEN);
+//
+//				if(!(GPIOE_PDIR & (1 << 6))){
+//					f_mount(0,&fs);
+//					res = f_open(&file, "0:/img.img", FA_OPEN_ALWAYS | FA_WRITE | FA_READ);
+//
+//					filesize = f_size(&file);
+//
+//					f_lseek(&file, filesize);
+//
+//					f_printf(&file, "img\n");
+//
+//					for(int i=0; i< 600; i++){
+//						f_printf(&file, "%d,", img_bin_buff[i]);
+//					}
+//					f_printf(&file, "\n");
+//
+//					f_close(&file);
+//					
+//					LCD_Str(strsite, (uint8 *)"SD write Ok!", BLUE, YELLOW);
+//				}
+//				break;
+//			default:
+//				break;
+//			}
+//		}
+//	}
 }
